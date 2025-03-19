6f97bd4bf20f1b2a9558652bb43eddbddafa5bce
Joe Niu <46828096+joe-niu@users.noreply.github.com>
2025-03-18 19:10:44 -0500

    carson's initial code for hdfs setup

---
+++ test_hdfs_client.py
@@ -0,0 +1,16 @@
+import unittest
+from DataToHDFS import HDFSClient
+
+class TestHDFSClient(unittest.TestCase):
+    def setUp(self):
+        self.client = HDFSClient()
+
+    def test_load_config(self):
+        self.assertIn('namenode_host', self.client.config)
+        self.assertIn('port', self.client.config)
+
+    # Additional tests can be added for upload/download methods
+    # For example, using a mock HDFS server or temporary files.
+
+if __name__ == '__main__':
+    unittest.main()
